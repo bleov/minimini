b3fb92467d67b8a18480e25a6d2f5e5da125db70
pika <pikapower9080@gmail.com>
2026-04-11 12:42:37 -0400

    wip: custom puzzle editor router

--- src/routes/custom/CreateRouter.tsx
+++ src/routes/custom/CreateRouter.tsx
@@ -1,3 +1,32 @@
+import { pb } from "@/main";
+import { lazy, useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+const CrosswordCreator = lazy(() => import("./crossword/Create"));
+const ConnectionsCreator = lazy(() => import("./connections/Create"));
+
 export default function CreateRouter() {
-  return <></>;
+  const params = useParams();
+  const [type, setType] = useState<"crossword" | "connections" | null>(null);
+
+  useEffect(() => {
+    if (params.id) {
+      pb.collection("custom_puzzles")
+        .getOne(params.id, { fields: "type" })
+        .then((res) => {
+          if (res.type === "connections") {
+            setType("connections");
+          } else {
+            setType("crossword");
+          }
+        });
+    }
+  }, []);
+
+  if (type === "connections") {
+    return <ConnectionsCreator />;
+  }
+  if (type === "crossword") {
+    return <CrosswordCreator />;
+  }
 }
---
+++ src/routes/custom/connections/Create.tsx
@@ -0,0 +1,3 @@
+export default function ConnectionsCreator() {
+  return <>Connections</>;
+}
