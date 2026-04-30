1b8067d7404be46eb9be0e9d968d996a525d601f
pika <pikapower9080@gmail.com>
2026-04-30 19:32:47 -0400

    feat: share/copy link button in details editor

--- src/routes/custom/Components/DetailsEditor.tsx
+++ src/routes/custom/Components/DetailsEditor.tsx
@@ -1,5 +1,6 @@
-import { LinkIcon, PencilIcon } from "lucide-react";
-import type { Dispatch, SetStateAction } from "react";
+import copy from "copy-to-clipboard";
+import { CheckIcon, LinkIcon, PencilIcon, ShareIcon } from "lucide-react";
+import { useState, type Dispatch, type SetStateAction } from "react";
 import { Button, Checkbox, CheckboxGroup, Form, Modal, Text, VStack } from "rsuite";
 
 interface DetailsValue {
@@ -15,6 +16,9 @@ interface DetailsEditorProps {
 }
 
 export default function DetailsEditor({ open, setOpen, details, setDetails }: DetailsEditorProps) {
+  const shareEnabled = "share" in navigator;
+  const [wasCopied, setWasCopied] = useState(false);
+
   return (
     <Modal
       open={open}
@@ -55,8 +59,29 @@ export default function DetailsEditor({ open, setOpen, details, setDetails }: De
                 </Form.Text>
               </Form.Control>
             </Form.Group>
-            <Button margin={1} startIcon={<LinkIcon />}>
-              Copy Share Link
+            <Button
+              margin={1}
+              startIcon={shareEnabled ? <ShareIcon /> : wasCopied ? <CheckIcon /> : <LinkIcon />}
+              onClick={() => {
+                if (!shareEnabled) {
+                  if (wasCopied) return;
+                  copy(window.location.href.replace("/edit", "")).then(() => {
+                    setWasCopied(true);
+                    setTimeout(() => {
+                      setWasCopied(false);
+                    }, 2000);
+                  });
+                } else {
+                  navigator
+                    .share({
+                      title: details.title,
+                      url: window.location.href.replace("/edit", "")
+                    })
+                    .catch(console.warn);
+                }
+              }}
+            >
+              {shareEnabled ? "Share Puzzle" : wasCopied ? "Copied" : "Copy Link"}
             </Button>
           </VStack>
         </Form>
