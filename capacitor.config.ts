147b7dccad741492ce0292823422cf1b298f61f0
pikapower9080 <pikapower9080@gmail.com>
2026-08-17 17:35:56 -0400

    chore: setup capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'net.bleov.glyph',
+  appName: 'Glyph',
+  webDir: 'dist'
+};
+
+export default config;
